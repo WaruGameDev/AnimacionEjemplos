c18410e2045b8775c3dd0b77e1cc31a2168335e7
WaruGameDev <60048774+WaruGameDev@users.noreply.github.com>
2025-09-24 10:58:04 -0300

    drag and drop

---
+++ Assets/Scripts/DragAndDrop/Kitchen.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Kitchen : MonoBehaviour
+{
+    public GameObject chef;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Rabbit"))
+        {
+            other.gameObject.SetActive(false);
+            chef.SetActive(true);
+        }
+
+    }
+}
